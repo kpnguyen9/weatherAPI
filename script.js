94f4a6b58006cb2f78336b09aaf0858c4518e034
kpnguyen9 <kpnguyen2@gmail.com>
2022-01-09 20:21:47 -0600

    retrieved weather with city input

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const inputBox = document.getElementById("box");
 const inputText = inputBox.value;
 const submitButton = document.getElementById("button");
 
+let cityName = "";
+
+const key = "3e4422d6e04655f7e63efa6a0266b041";
+
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
-  let number = "";
+  let cityName = "";
 
-  number = inputBox.value;
-  console.log(number);
+  cityName = inputBox.value;
+  console.log(cityName);
 
-  let url = `https://swapi.dev/api/people/${number}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`;
 
   console.log(url);
 
