e664f87c87b9f146963fb2eaff1ce810b75fe3df
kpnguyen9 <kpnguyen2@gmail.com>
2022-01-09 22:10:52 -0600

    printed temp info into list

--- script.js
+++ script.js
@@ -3,17 +3,41 @@ const submitButton = document.getElementById("button");
 
 const key = "3e4422d6e04655f7e63efa6a0266b041";
 
+const logData = (data) => {
+  console.dir(data);
+  return data;
+};
+
+const extractMain = (cityInfo) => {
+  const { main } = cityInfo;
+  console.log(main);
+  return main;
+};
+
+const tempDiv = document.getElementById("temperature");
+
+const printTemps = (temps) => {
+  for (let a in temps) {
+    console.log(`${a}: ${temps[a]}`);
+
+    let newLi = document.createElement("li");
+    newLi.innerHTML = `${a}: ${temps[a]}`;
+    tempDiv.appendChild(newLi);
+  }
+};
+
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
+  tempDiv.innerHTML = "";
   let cityName = inputBox.value;
-
   console.log(cityName);
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`;
-
   console.log(url);
 
   fetch(url)
     .then((response) => response.json())
-    .then((data) => console.dir(data));
+    .then(logData)
+    .then(extractMain)
+    .then(printTemps);
 });
