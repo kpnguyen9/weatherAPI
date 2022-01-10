68719e387d2d3bcdb9d1835257e66ebdb3ae5748
kpnguyen9 <kpnguyen2@gmail.com>
2022-01-09 20:31:00 -0600

    refactored to clean up code

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 const inputBox = document.getElementById("box");
-const inputText = inputBox.value;
 const submitButton = document.getElementById("button");
-
-let cityName = "";
 
 const key = "3e4422d6e04655f7e63efa6a0266b041";
 
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
-  let cityName = "";
+  let cityName = inputBox.value;
 
-  cityName = inputBox.value;
   console.log(cityName);
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`;
